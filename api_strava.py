75bbc146009471ba53161aa6e0bd61232c98ff43
Luke Bernstein <lnbernstein13@gmail.com>
2022-07-23 15:15:25 -0400

    Created Strava API Connection

--- api_strava.py
+++ api_strava.py
@@ -1,4 +1,10 @@
 import requests
+import urllib3
+import pandas as pd
+from pandas.io.json import json_normalize
+
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 auth_url = "https://www.strava.com/oauth/token"
 activites_url = "https://www.strava.com/api/v3/athlete/activities"
@@ -6,16 +12,27 @@ activites_url = "https://www.strava.com/api/v3/athlete/activities"
 payload = {
     'client_id': "90249",
     'client_secret': '3ce3501c22aed4fbcd6475e48b57af686f1b17be',
-    'refresh_token': '782dbf689a067a29c8c0b71806e36b230767bbc8',
+    # 'refresh_token': '782dbf689a067a29c8c0b71806e36b230767bbc8',
+    'refresh_token': '2f1ba6111a7c67ee301cfb9505530796ff26db6b',
     'grant_type': "refresh_token",
     'f': 'json'
 }
 
-print("Requesting Token...\n")
+# print("Requesting Token...\n")
 res = requests.post(auth_url, data=payload, verify=False)
+# print(res.json())
 access_token = res.json()['access_token']
-print("Access Token = {}\n".format(access_token))
+# print("Access Token = {}\n".format(access_token))
 
 header = {'Authorization': 'Bearer ' + access_token}
 param = {'per_page': 200, 'page': 1}
 my_dataset = requests.get(activites_url, headers=header, params=param).json()
+
+# print(my_dataset)
+
+data = json_normalize(my_dataset)
+print(data.columns)
+print(data.shape)
+
+print(data.head())
+print(type(data))
