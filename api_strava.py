5252047b619f8acafc0e003503715d70afa3f80b
Luke Bernstein <lnbernstein13@gmail.com>
2022-07-23 13:45:32 -0400

    Initial Commit

---
+++ api_strava.py
@@ -0,0 +1,21 @@
+import requests
+
+auth_url = "https://www.strava.com/oauth/token"
+activites_url = "https://www.strava.com/api/v3/athlete/activities"
+
+payload = {
+    'client_id': "90249",
+    'client_secret': '3ce3501c22aed4fbcd6475e48b57af686f1b17be',
+    'refresh_token': '782dbf689a067a29c8c0b71806e36b230767bbc8',
+    'grant_type': "refresh_token",
+    'f': 'json'
+}
+
+print("Requesting Token...\n")
+res = requests.post(auth_url, data=payload, verify=False)
+access_token = res.json()['access_token']
+print("Access Token = {}\n".format(access_token))
+
+header = {'Authorization': 'Bearer ' + access_token}
+param = {'per_page': 200, 'page': 1}
+my_dataset = requests.get(activites_url, headers=header, params=param).json()
---
+++ main.py
@@ -0,0 +1,58 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import streamlit.components.v1 as components
+
+
+
+
+weekly_mileage = 10
+runs_completed = 1
+runs_of_week = 4
+
+def generate_run(weekly_mileage, runs_completed, runs_of_week):
+    miles_per_run = weekly_mileage/runs_of_week
+    if(runs_completed != runs_of_week-1):
+        return miles_per_run
+    else:
+        return miles_per_run*1.75
+
+
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+bpm = 172
+bpm_change = 4
+
+st.markdown("<h1 style='text-align: left;'>Becoming a Better Athlete</h1>", unsafe_allow_html=True)
+st.markdown("<p style='text-align: left;'>Following the 10% rule, this application will automatically generate a run based on prior training</p>", unsafe_allow_html=True)
+
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.write(' ')
+
+with col2:
+    st.metric("    Today's Mileage", generate_run(weekly_mileage, runs_completed, runs_of_week))
+
+with col3:
+    st.write(' ')
+
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.metric('Weekly Mileage', 42, 2)
+
+with col2:
+    st.metric('Average Heart Rate', bpm, bpm_change)
+
+with col3:
+    st.metric('Average Pace', bpm, bpm_change)
+
+
+st.line_chart(chart_data)
+components.iframe("https://calendar.google.com/calendar/embed?src=c_mst8mkd08jsbukco439oht7gr8%40group.calendar.google.com&ctz=America%2FNew_York",
+                    width=800, height=500, scrolling=True)
