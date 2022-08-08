36f545085af3ef3715b8958d69da4bcfeaca446a
Luke Bernstein <lnbernstein13@gmail.com>
2022-08-08 10:55:45 -0400

    adding github logo and code link

--- Home.py
+++ Home.py
@@ -91,6 +91,9 @@ st.sidebar.image(image)
 
 st.sidebar.write('Check out my [resume!](https://drive.google.com/file/d/1VvNS4UlRF0X-LvJ3iG0wQHrq_CkIoHik/view?usp=sharing)')
 
+st.markdown("[![Foo](GitHub-Mark-64px.png)](https://github.com/lnbernstein/running_tracker)")
+
+
 
 
 # rest of page
